9c645ab7bc818445e79c116dfb0512fc90fcbde0
anandprasadsingh <anandprasadsingh@exponentialai.com>
2025-05-23 12:18:18 +0530

    modified host and port to render

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from app.utils.db_utils.database import Base, engine
 from app.api.auth import auth_router
 from app.api.events import events_router
 import uvicorn
+import os
 
 
 # Create all DB tables
@@ -16,5 +17,6 @@ app.include_router(events_router,prefix="/api/events", tags=["Events"])
 bearer_scheme = HTTPBearer()
 
 if __name__ == '__main__':
-    uvicorn.run('main:app',reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=False)
 
